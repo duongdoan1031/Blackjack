2f3d7ed2303881db91d97fddbf8d1e6d8bc0faf7
duongdoan1031 <duongdoan1031@gmail.com>
2016-04-19 16:10:27 -0700

    Update Player.h

--- Player.h
+++ Player.h
@@ -6,12 +6,14 @@
 class Player
 {
 public:
+	char generateCard();
 	int getPoints();
 	void addCard(card a);
 	int calculatePoints();
 	bool cardCheck();
 private:
 	card hand[5];
+	char cardSymbol;
 	int points;
 	int cardsInHand;
 };
