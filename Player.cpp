94a04c0ac8a5848658ee274a1700e7a8b847f2c0
duongdoan1031 <duongdoan1031@gmail.com>
2016-04-19 16:15:27 -0700

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -6,6 +6,69 @@ Player::Player()
 	cardsInHand = 0;
 }
 
+char Player::generateCard(){
+	int item;
+	
+	item=rand() % 13 + 1;
+	
+	switch(item){
+		case 1:
+		cardSymbol = 'A';
+		break;
+		
+		case 2:
+		cardSymbol = '2';
+		break;
+		
+		case 3:
+		cardSymbol = '3';
+		break;
+		
+		case 4:
+		cardSymbol = '4';
+		break;
+		
+		case 5 :
+		cardSymbol = '5';
+		break;
+		
+		case 6:
+		cardSymbol = '6';
+		break;
+		
+		case 7:
+		cardSymbol = '7';
+		break;
+		
+		case 8:
+		cardSymbol = '8';
+		break;
+		
+		case 9:
+		cardSymbol = '9';
+		break;
+		
+		case 10:
+		cardSymbol = '0';
+		break;
+		
+		case 11:
+		cardSymbol = 'J';
+		break;
+		
+		case 12:
+		cardSymbol = 'Q';
+		break;
+		
+		
+		case 13:
+		cardSymbol = 'K';
+		break;
+	}
+	
+	return cardSymbol;
+}
+
 void Player::addCard(card a)
 {
 	cardsInHand++;
